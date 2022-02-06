8b7e99fe79cdefd79952b1cbf55f7cb86bdc316a
穏便派ABAT教信者のGithub <93266683+ABATbeliever@users.noreply.github.com>
2022-02-06 22:26:28 +0900

    Add files via upload

---
+++ Internet Stroller sourse/Version 1.3.5/checker.bat
@@ -0,0 +1,61 @@
+@echo off
+title BootfileChecker
+set e=0
+if exist "InternetStroller.exe" (
+  echo ^[Found^]InternetStroller.exe
+) else (
+  echo ^[NotFound^]InternetStroller.exe
+  set e=1
+)
+cd app
+
+if exist "InternetStrollerBrowse.exe" (
+  echo ^[Found^]InternetStrollerBrowse.exe
+) else (
+  echo ^[NotFound^]InternetStrollerBrowse.exe
+  set e=1
+)
+if exist "console.exe" (
+  echo ^[Found^]console.exe
+) else (
+  echo ^[NotFound^]console.exe
+  set e=1
+)
+if exist "favo.bat" (
+  echo ^[Found^]favo.bat
+) else (
+  echo ^[NotFound^]favo.bat
+  set e=1
+)
+if exist "favorite.txt" (
+  echo ^[Found^]favorite.txt
+) else (
+  echo ^[NotFound^]favorite.txt
+  set e=1
+)
+if exist "main.jpg" (
+  echo ^[Found^]main.jpg
+) else (
+  echo ^[NotFound^]main.jpg
+  set e=1
+)
+if exist "Splash.jpg" (
+  echo ^[Found^]Splash.jpg
+) else (
+  echo ^[NotFound^]Splash.jpg
+  set e=1
+)
+echo.
+if %e% == 0 (echo OK
+) else (
+  echo ^【警告^】
+  echo 大切なファイルが見つかりません
+  echo 再ダウンロードを推奨します
+  echo.
+  echo I can't find my important file
+  echo Re-download recommended
+  pause
+  exit
+)
+pause
+exit
